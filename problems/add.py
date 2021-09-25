22f22f36da5150fc463247d89fe09327cb53b06e
Gaurav Singh <86864026+ghacker1302@users.noreply.github.com>
2021-09-25 19:40:02 +0530

    Create add.py

---
+++ problems/add.py
@@ -0,0 +1,6 @@
+#Program to add two entered integer values.
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a+b
+print(f"sum of given numbers {a} , {b} is ",c)
