96af57c4564f42dc06128face895952624574e95
Gaurav Singh <86864026+ghacker1302@users.noreply.github.com>
2021-10-24 11:58:09 +0530

    Create lexicographical_order.py

---
+++ string/lexicographical_order.py
@@ -0,0 +1,16 @@
+a = input("enter the string: ")
+c = a.split() 
+c.sort()
+for i in c:
+    
+    print(i,end=" ")
+    
+#for descending lexicographical_order  
+a = input("enter the string: ")
+c = a.split(Reverse=True) 
+c.sort()
+for i in c:
+    
+    print(i,end=" ")
+    
+ 
